40fde50c3781b3ee0943c949384ccc3e0749e4e5
Kasaewa <Julia.kasaewa@gmail.com>
2025-02-21 20:48:09 +0100

    Dodano folder

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+};
+
+export default nextConfig;
