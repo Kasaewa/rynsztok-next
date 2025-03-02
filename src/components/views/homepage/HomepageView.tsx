9e3e68ff5712645db76bbff533427e7eaa6d6c61
Kasaewa <34487783+Kasaewa@users.noreply.github.com>
2025-03-02 08:31:54 +0100

    Update HomepageView.tsx

--- src/components/views/homepage/HomepageView.tsx
+++ src/components/views/homepage/HomepageView.tsx
@@ -1,24 +1,23 @@
-// kod dla strony głównej
-
 import styles from "./styles/HomepageView.module.scss";
+import Link from "next/link";
 
 export const HomepageView = () => {
   return (
     <section className={styles.homepage}>
       <h1 className={styles.title}>Legendy Rynsztoka</h1>
-        
-        <div className={styles.homepage-container}>
-          <Link href="/login"><button className={styles.button-login}>
-            Logowanie
-          </button></Link>
-          <button className={styles.button-login}>
-            <Link href="/register">Rejestracja</Link>
-          </button>
-            
-      <div className={styles.down-container}>
+
+      <div className={styles.homepageContainer}>
+        <Link href="/login">
+          <button className={styles.buttonLogin}>Logowanie</button>
+        </Link>
+        <Link href="/register">
+          <button className={styles.buttonLogin}>Rejestracja</button>
+        </Link>
+      </div>
+
+      <div className={styles.downContainer}>
         <p>Wchodzisz na własne ryzyko!</p>
       </div>
     </section>
   );
 };
-}
